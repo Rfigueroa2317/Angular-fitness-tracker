2110ed45046c9e86ece54668ce4e17c10a80cde4
Ricardo Figueroa <rfigueroa2317@gmail.com>
2022-12-29 10:57:42 -0500

    fixing duration bug

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { TrainingService } from '../training.service';
 
@@ -7,24 +7,27 @@ import { StopTrainingComponent } from './stop-training.component';
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
-  styleUrls: ['./current-training.component.css']
+  styleUrls: ['./current-training.component.css'],
 })
 export class CurrentTrainingComponent implements OnInit {
-  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: number;
 
-  constructor(private dialog: MatDialog, private trainingService: TrainingService) { }
+  constructor(
+    private dialog: MatDialog,
+    private trainingService: TrainingService
+  ) {}
 
   ngOnInit(): void {
     this.startorResumeTimer();
   }
 
   startorResumeTimer() {
-    const step = this.trainingService.getRunningExercise().duration / 1000;
+    const step = this.trainingService.getRunningExercise().duration / 0.05; // setting the duration to .05 will make the timer go a reasonable pace
     this.timer = setInterval(() => {
       this.progress = this.progress + 1;
       if (this.progress >= 100) {
+        this.trainingService.completeExercise();
         clearInterval(this.timer);
       }
     }, step);
@@ -34,16 +37,16 @@ export class CurrentTrainingComponent implements OnInit {
     clearInterval(this.timer);
     const dialogRef = this.dialog.open(StopTrainingComponent, {
       data: {
-        progress: this.progress
-    }
-  });
+        progress: this.progress,
+      },
+    });
 
-  dialogRef.afterClosed().subscribe(result => {
-    if (result) {
-      this.trainingExit.emit();
-    } else {
-      this.startorResumeTimer();
-    }
-  })
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.trainingService.cancelExercise(this.progress);
+      } else {
+        this.startorResumeTimer();
+      }
+    });
   }
 }
